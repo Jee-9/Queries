dfc626e0c038f36dc870822f53fd68d901b5dad9
Jee-9 <jee161209@gmail.com>
2024-12-06 16:35:45 +0900

    프로그래머스 lv.4

--- 특정 세대의 대장균 찾기.sql
+++ 특정 세대의 대장균 찾기.sql
@@ -1,10 +1,10 @@
-# 2세대 찾기
+-- 2세대 찾기
 select *
 from ecoli_data
 where parent_id is not null
     and parent_id IN (select id from ecoli_data where parent_id is null)
     
-# 3세대 찾기
+-- 3세대 찾기
 SELECT 
     ID
 FROM
